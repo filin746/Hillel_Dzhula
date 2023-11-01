68a766b587a2fec5db872a014e878f3bf955e7eb
Vladislav Dzhula <99653939+filin746@users.noreply.github.com>
2023-11-01 17:55:44 +0200

    Delete lesson_5 directory

--- lesson_5/ClassWork/3.py
+++
@@ -1,9 +0,0 @@
-# name = "John"
-# age = 30
-
-name, age = "John", 30
-
-print(f"{name=}")
-print(f"{age=}")
-
-# print(f"{john=}")
--- lesson_5/ClassWork/6.py
+++
@@ -1,5 +0,0 @@
-my_list = ['apple', "table", "bred"]
-
-v1, v2, v3 = my_list
-
-print(*my_list, sep="-")
